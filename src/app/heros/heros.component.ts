ec43e2bcd81572a1b80b4b59544d09be39162cbe
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-12-18 00:14:55 +0900

    add HeroService

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heros',
@@ -8,22 +9,15 @@ import { Hero } from '../Hero';
 })
 export class HerosComponent implements OnInit {
   currentHero: Hero;
-  heros: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+  heros: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+  }
 
   ngOnInit() {
+    this.heroService.getHeros().subscribe(heros => {
+      this.heros = heros;
+    })
   }
 
   setCurrentHero(hero: Hero) {
